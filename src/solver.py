10f170e5fdea8bceb510bdafcad280d805803296
Mark Schlutow <mschlutow@bgc-jena.mpg.de>
2025-03-02 21:37:43 +0100

    Background and surface concentration added to return.

--- src/solver.py
+++ src/solver.py
@@ -12,7 +12,7 @@ def ivp_solver( fftp0, fftq0, u, v, K, z, Lx, Ly, method='SIE' ):
     with the Fast Fourier Transform
     '''
 
-    fftp, fftq = np.copy(fftp0), np.copy(fftq0)
+    fftpm, fftqm = np.copy(fftp0), np.copy(fftq0)
 
     nz = len(z)
     dz = np.diff(z,axis=0)
@@ -26,9 +26,9 @@ def ivp_solver( fftp0, fftq0, u, v, K, z, Lx, Ly, method='SIE' ):
         # exponential integrator (exact) method
         if method == 'EI':
             eig = np.sqrt( Ti*Kinv )
-            dum = np.cos(eig*dzi) * fftp - Kinv/eig*np.sin(eig*dzi) * fftq
-            fftq = Ti/eig*np.sin(eig*dzi) * fftp + np.cos(eig*dzi) * fftq
-            fftp = dum
+            dum = np.cos(eig*dzi) * fftpm - Kinv/eig*np.sin(eig*dzi) * fftqm
+            fftqm = Ti/eig*np.sin(eig*dzi) * fftpm + np.cos(eig*dzi) * fftqm
+            fftpm = dum
 
         # Taylor series for exponential integrator method up to 3rd order
         if method == 'TSEI3':
@@ -37,22 +37,22 @@ def ivp_solver( fftp0, fftq0, u, v, K, z, Lx, Ly, method='SIE' ):
             c = Ti * dzi - 1.0/6.0 * Kinv * Ti**2 *dzi**3
             d = 1.0 - 0.5 * Kinv * Ti * dzi**2
 
-            dum  = a * fftp + b * fftq
-            fftq = c * fftp + d * fftq
-            fftp = dum
+            dum   = a * fftpm + b * fftqm
+            fftqm = c * fftpm + d * fftqm
+            fftpm = dum
 
         # Semi-implicit Euler method
         if method == 'SIE':
-            fftp = fftp - dzi * Kinv * fftq
-            fftq = fftq + dzi * Ti * fftp
+            fftpm = fftpm - dzi * Kinv * fftqm
+            fftqm = fftqm + dzi * Ti * fftpm
 
         # Explicit Euler method
         if method == 'EE':
-            dum = fftp - dz[i]/K[i]*fftq
-            fftq = fftq + dz[i]*Ti*fftp
-            fftp = dum
+            dum = fftpm - dz[i]/K[i]*fftqm
+            fftqm = fftqm + dz[i]*Ti*fftpm
+            fftpm = dum
 
-    return fftp, fftq
+    return fftpm, fftqm
 
 
 def steady_state_transport_solver(u, v, K, z, 
@@ -69,20 +69,37 @@ def steady_state_transport_solver(u, v, K, z,
     given vertical profiles of wind and eddy diffusivity
     using the Fourier, linear shooting and semi-implicit Euler methods
 
-    Paramters:
-        p000 (float): laterally averaged concentration at z=z0 [1]
-        q0 (float): kinematic flux boundary condition at z=z0 [m s-1]
-        u,v (float): zonal and meridional wind [m s-1]
-        K (float): eddy diffusivity [m2 s-1]
-        z (float): grid points [m]
-        nlx,nly (int): number of Fourier modes
-        dx,dy (float): grid increments [m]
-        fetch (float): zero-flux halo around domain [m]
-        green (bool): returns Green's function if True
+    Parameters:
+        p000: scalar(float), optional 
+            Background concentration at z=z0 [U]
+        q0: array(float)
+            2D field of surface kinematic flux at z=z0 [U m s-1]
+        u,v: array(float) 
+            1D array of zonal and meridional wind vertical profile [m s-1]
+        K: array(float)
+            1D array of eddy diffusivity [m2 s-1]
+        z: array(float): 
+            1D array of vertical grid points from z0 to zm [m]
+        nlx, nly: scalar(int) 
+            Number of Fourier modes
+        dx, dy: scalar(float) 
+            Grid increments [m]
+        fetch: scalar(float), optional 
+            Width of zero-flux halo around domain [m]
+        green: scalar(bool), optional 
+            Activates Green's function for output
+        analytic: scalar(bool), optional
+            Analytic solution for constant wind and eddy diffusivity
 
     Returns:
-        p (float): concentration at zm
-        q (float): kinematic flux at zm.
+        p0: array(float)
+            2D field of surface concentrations
+        pm00: scalar(float) 
+            Background concentration at zm
+        pm: array(float) 
+            2D field of concentration at zm
+        qm: array(float) 
+            2D field of kinematic flux at zm.
     """
     
     # number of grid cells
@@ -93,7 +110,7 @@ def steady_state_transport_solver(u, v, K, z,
     py = int(fetch/dy)
 
     # construct zero-flux halo by padding
-    q0  = np.pad( q0, ((py,py),(px,px)), mode='constant', constant_values=0.0 )
+    q0  = np.pad(q0, ((py,py),(px,px)), mode='constant', constant_values=0.0)
 
     # extent domain
     nx = nx + 2*px
@@ -108,31 +125,25 @@ def steady_state_transport_solver(u, v, K, z,
     dlx, dly = (nx-nlx)//2, (ny-nly)//2
 
     if green:
+        # Fourier trafo of delta distribution
         tfftq0 = np.ones((nly,nlx),dtype=complex)/nx/ny
     else:
         fftq0 = fft.fft2(q0,norm='forward') # fft of source
 
-        # truncate fourier series by removing higher-frequency components
-        tfftq0 = np.zeros((nly, nlx), dtype=complex)
-
         # shift zero wave number to center of array
         fftq0 = fft.fftshift(fftq0)
 
-        # truncate
+        # truncate fourier series by removing higher-frequency components
         tfftq0 = fftq0[dly:ny-dly,dlx:nx-dlx]
 
-        # shift back
+        # unshift
         tfftq0 = fft.ifftshift(tfftq0)
 
-    # initialization
-    tfftp  = np.zeros((nly,nlx),dtype=complex) 
-    tfftq  = np.zeros((nly,nlx),dtype=complex) # initialization
-
     # Fourier summation index
-    ilx = fft.fftfreq( nlx, d=1.0/nlx  ) 
-    ily = fft.fftfreq( nly, d=1.0/nly  ) 
+    ilx = fft.fftfreq(nlx, d=1.0/nlx) 
+    ily = fft.fftfreq(nly, d=1.0/nly) 
 
-    # wavenumbers
+    # zonal and meridional wavenumbers
     lx = 2.0*np.pi/dx/nx * ilx 
     ly = 2.0*np.pi/dy/ny * ily 
 
@@ -140,9 +151,6 @@ def steady_state_transport_solver(u, v, K, z,
 
     dz = np.diff(z,axis=0)
     nz = len(z)
-
-    # solve boundary value problem for (n,m) =/= (0,0) 
-    # use linear shooting method
 
     # define mask to seperate degenerated and non-degenerated system
     msk      = np.ones((nly,nlx),dtype=bool) # all n and m not equal 0
@@ -153,65 +161,90 @@ def steady_state_transport_solver(u, v, K, z,
 
     Kinv = 1.0 / K[nz-1]
 
+    # Eigenvalue determining solution for z > zm
     eigval = np.sqrt( 
         Lx[msk]**2 + Ly[msk]**2 
        +1j * u[nz-1] * Kinv * Lx[msk] 
        +1j * v[nz-1] * Kinv * Ly[msk]
                      )
-    tfftq[0,0] = tfftq0[0,0] # conservation by design
+    # initialization
+    tfftp0 = np.zeros((nly,nlx),dtype=complex) 
+    tfftpm = np.zeros((nly,nlx),dtype=complex) 
+    tfftqm = np.zeros((nly,nlx),dtype=complex)
+
+    tfftp0[0,0] = p000
+    tfftqm[0,0] = tfftq0[0,0] # conservation by design
 
     if analytic:
 
         # constant profiles solution
         # for validation purposes
-        dz = z[nz-1]-z[0]
-        tfftp[0,0] = p000 - tfftq0[0,0] * Kinv  * dz
-        tfftq[msk] = tfftq0[msk] * np.exp( -eigval * dz )
-        tfftp[msk] = tfftq[msk] * Kinv / eigval 
+        h = z[nz-1]-z[0]
+        tfftp0[msk] = tfftq0[msk] * Kinv / eigval 
+        tfftpm[0,0] = p000 - tfftq0[0,0] * Kinv  * h
+        tfftqm[msk] = tfftq0[msk] * np.exp(-eigval * h)
+        tfftpm[msk] = tfftqm[msk] * Kinv / eigval 
     
     else:                                        
     
         # solve non-degenerated problem for (n,m) =/= (0,0)
-        # by two auxillary initial value problems  
+        # by linear shooting method
+        # use two auxillary initial value problems  
         tfftp1, tfftq1 = ivp_solver(one, zero,       u,v,K,z,Lx[msk],Ly[msk])
         tfftp2, tfftq2 = ivp_solver(zero,tfftq0[msk],u,v,K,z,Lx[msk],Ly[msk])
                                                  
-        # linear combination of the two solution of the IVP 
-        alpha = -( tfftq2 - K[nz-1]*eigval*tfftp2 ) \
-               / ( tfftq1 - K[nz-1]*eigval*tfftp1 )
+        alpha = -(tfftq2 - K[nz-1]*eigval*tfftp2) \
+               / (tfftq1 - K[nz-1]*eigval*tfftp1)
 
-        tfftp[msk]  = alpha * tfftp1 + tfftp2        
-        tfftq[msk]  = alpha * tfftq1 + tfftq2        
+        # linear combination of the two solution of the IVP 
+        tfftp0[msk] = alpha       
+        tfftpm[msk] = alpha * tfftp1 + tfftp2        
+        tfftqm[msk] = alpha * tfftq1 + tfftq2        
                                                  
         # solve degenerated problem for (n,m) =  (0,0)
         # with Euler forward method
-        tfftp[0,0]  = p000                         
+        tfftpm[0,0] = p000                         
         for i in range(nz-1):                    
-            tfftp[0,0] = tfftp[0,0] - tfftq0[0,0] / K[i] * dz[i]
+            tfftpm[0,0] = tfftpm[0,0] - tfftq0[0,0] / K[i] * dz[i]
+
+    # shift zero to center
+    tfftp0  = fft.fftshift(tfftp0)
+    tfftpm  = fft.fftshift(tfftpm)
+    tfftqm  = fft.fftshift(tfftqm)
 
     # untruncate
-    tfftp  = fft.fftshift(tfftp)
-    tfftq  = fft.fftshift(tfftq)
-    fftp   = np.pad(tfftp, ((dly,dly),(dlx,dlx)), mode='constant', constant_values=0.0)
-    fftq   = np.pad(tfftq, ((dly,dly),(dlx,dlx)), mode='constant', constant_values=0.0)
-    fftp   = fft.ifftshift(fftp)
-    fftq   = fft.ifftshift(fftq)
+    fftp0 = np.pad(tfftp0, ((dly,dly),(dlx,dlx)), mode='constant', constant_values=0.0)
+    fftpm = np.pad(tfftpm, ((dly,dly),(dlx,dlx)), mode='constant', constant_values=0.0)
+    fftqm = np.pad(tfftqm, ((dly,dly),(dlx,dlx)), mode='constant', constant_values=0.0)
 
-    p  = fft.ifft2(fftp,norm='forward').real # concentration  
-    q  = fft.ifft2(fftq,norm='forward').real # kinematic flux 
+    # unshift
+    fftp0 = fft.ifftshift(fftp0)
+    fftpm = fft.ifftshift(fftpm)
+    fftqm = fft.ifftshift(fftqm)
+
+    p0 = fft.ifft2(fftp0,norm='forward').real # concentration  
+    pm = fft.ifft2(fftpm,norm='forward').real # concentration  
+    qm = fft.ifft2(fftqm,norm='forward').real # kinematic flux 
 
     if green:
-        p  = np.roll(p,(ny//2,nx//2),axis=(0,1))
-        q  = np.roll(q,(ny//2,nx//2),axis=(0,1))
+        p0 = np.roll(p0,(ny//2,nx//2),axis=(0,1))
+        pm = np.roll(pm,(ny//2,nx//2),axis=(0,1))
+        qm = np.roll(qm,(ny//2,nx//2),axis=(0,1))
 
-    return p[py:ny-py,px:nx-px], q[py:ny-py,px:nx-px]
+    return p0[py:ny-py,px:nx-px], fftpm[0,0].real, \
+           pm[py:ny-py,px:nx-px], qm[py:ny-py,px:nx-px]
 
 
 def convolve( f, g, iy, ix):
+    """
+    Computes the convolution of two 2D arrays evaluated at (ix,iy).
+    scipy.convolve2d(...,mode='valid') is slighly faster,
+    but gives less precise results.
+    """
 
     ny, nx = g.shape
                            
-    g = np.roll(g,(ny//2-iy,nx//2-ix),axis=(0,1))
+    g = np.roll(g, (ny//2-iy,nx//2-ix), axis=(0,1))
 
     return np.sum(f*g)
 
@@ -221,9 +254,9 @@ if __name__=='__main__':
     import matplotlib.pyplot as plt
 
     nx, ny, nz    = 1024, 512, 20
-    nlx, nly      = 512, 256  
+    nlx, nly      = 1024, 512 
     xmx, ymx, zmx = 2000.0, 1000.0, 10.0
-    fetch         = 2000.0
+    fetch         = 1000.0
     xm, ym        = 1500.0, 700.0
     um, vm        = 1.2, 0.5
     ustar, mol    = 0.25, 100.0
@@ -251,9 +284,20 @@ if __name__=='__main__':
     # direct computation with constant profile
     z, u, v, K = vertical_profiles(nz, zmx, um, vm, ustar, constant=True)
     tic = time.time()
-    p, q = steady_state_transport_solver(u,v,K,z,dx,dy,nlx,nly,q0,p000,analytic=True,fetch=fetch)
+    p0,pm00,pm, qm = steady_state_transport_solver(u,v,K,z,
+                                                   dx,dy,nlx,nly,
+                                                   q0,p000,
+                                                   analytic=True,
+                                                   fetch=fetch)
     toc = time.time()
-    plt.imshow(p,origin="lower",extent=[0,xmx,0,ymx])
+    plt.imshow(p0,origin="lower",extent=[0,xmx,0,ymx])
+    plt.title("Concentration at z0")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.plot(xm,ym,'ro') 
+    plt.colorbar()
+    plt.show()
+    plt.imshow(pm,origin="lower",extent=[0,xmx,0,ymx])
     # plt.contour(X,Y,p)
     plt.title("Concentration at zm for constant profile")
     plt.xlabel("x")
@@ -263,22 +307,33 @@ if __name__=='__main__':
     plt.show()
     print('Constant profile')
     print('time ',toc-tic,'s')
-    print('p    = ',p[iy,ix])
-    print('q    = ',q[iy,ix])
+    print('pm00          = ',pm00)
+    print('pm at xm,ym   = ',pm[iy,ix])
+    print('qm at xm,ym   = ',qm[iy,ix])
  
     # direct computation 
     tic = time.time()
     z, u, v, K = vertical_profiles(nz, zmx, um, vm, ustar, mol, constant=True)
-    p, q = steady_state_transport_solver(u,v,K,z,dx,dy,nlx,nly,q0,p000,fetch=fetch)
+    p0, pm00, pm, qm = steady_state_transport_solver(u,v,K,z,
+                                                     dx,dy,nlx,nly,
+                                                     q0,p000,
+                                                     fetch=fetch)
     toc = time.time()
-    plt.imshow(p,origin="lower",extent=[0,xmx,0,ymx])
+    plt.imshow(p0,origin="lower",extent=[0,xmx,0,ymx])
+    plt.title("Concentration at z0")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.plot(xm,ym,'ro') 
+    plt.colorbar()
+    plt.show()
+    plt.imshow(pm,origin="lower",extent=[0,xmx,0,ymx])
     plt.title("Concentration at zm")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.plot(xm,ym,'ro') 
     plt.colorbar()
     plt.show()
-    plt.imshow(q,origin="lower",extent=[0,xmx,0,ymx])
+    plt.imshow(qm,origin="lower",extent=[0,xmx,0,ymx])
     plt.title("Vertical kinematic flux at zm")
     plt.xlabel("x")
     plt.ylabel("y")
@@ -287,22 +342,26 @@ if __name__=='__main__':
     plt.show()
     print('Direct method')
     print('time ',toc-tic,'s')
-    print('p    = ',p[iy,ix])
-    print('q    = ',q[iy,ix])
+    print('pm00         = ',pm00)
+    print('pm at xm,ym  = ',pm[iy,ix])
+    print('qm at xm,ym  = ',qm[iy,ix])
 
     # compute Green function by upgraded solver
     dum = np.zeros([ny,nx]) # dummy for dimensions
-    pg, qg = steady_state_transport_solver(u,v,K,z,dx,dy,nlx,nly,q0=dum,green=True,fetch=fetch)
+    _,_,pg, qg = steady_state_transport_solver(u,v,K,z,
+                                               dx,dy,nlx,nly,
+                                               q0=dum,green=True,
+                                               fetch=fetch)
 
     # compute solution by convolution with Green function
     tic = time.time()
-    p = p000 + convolve(q0,pg,iy,ix)
-    q = convolve(q0,qg,iy,ix)
+    pm = p000 + convolve(q0,pg,iy,ix)
+    qm = convolve(q0,qg,iy,ix)
     toc = time.time()
     print('Convolution with Green function')
     print('time ',toc-tic,'s')
-    print('p    = ',p)
-    print('q    = ',q)
+    print('pm    = ',pm)
+    print('qm    = ',qm)
 
     # Scipy convolution with Green function
     # tic = time.time()
