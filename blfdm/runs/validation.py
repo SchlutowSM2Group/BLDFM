0a77bbaed366f67eb6b7e649d81a6c6a939456e8
Mark Schlutow <mschlutow@bgc-jena.mpg.de>
2025-03-03 14:23:25 +0100

    First attempts for validation

--- blfdm/runs/validation.py
+++ blfdm/runs/validation.py
@@ -1,7 +1,11 @@
 import numpy as np
-# import scipy.fft as fft
-# import time
+import pandas as pd
+
 from ..src.most import vertical_profiles
 from ..src.solver import steady_state_transport_solver
-# from scipy.signal import convolve2d
+
+file_path = "./data/footprint model experiment data/03-24 basic data base from matlab.csv"
+df = pd.read_csv(file_path, na_values='<missing>')
+
+print(df.var)
 
