73419ab1e214326f92d4aea55143812da00a0168
Mark Schlutow <mschlutow@bgc-jena.mpg.de>
2025-03-23 21:22:40 +0100

    datafile io

--- blfdm/runs/Rey-Sanchez_et_al_2022.py
+++ blfdm/runs/Rey-Sanchez_et_al_2022.py
@@ -3,9 +3,23 @@ import pandas as pd
 
 from ..src.most import vertical_profiles
 from ..src.solver import steady_state_transport_solver
+import scipy.io
 
-file_path = "./data/footprint model experiment data/03-24 basic data base from matlab.csv"
-df = pd.read_csv(file_path, na_values='<missing>')
+# df = pd.read_csv(file_path, na_values='<missing>')
 
-print(df.var)
+# Read data file
+file_path = "./data/Rey-Sanchez_et_al_2022/Aug_day_2019228_to_2019238_20Hz_L1_15min.mat"
+df = scipy.io.loadmat(file_path)
+
+time  = df['data']['time'][0][0] 
+qm    = df['data']['wm'][0][0]   # CH4 flux (after all corrections) [nmol CH4 m-2 s-1']
+wd    = df['data']['WD'][0][0]   # wind direction [degrees clockwise from true north]
+ubar  = df['data']['ubar'][0][0] # average wind speed
+ustar = df['data']['ustar'][0][0] 
+mol   = df['data']['L'][0][0]
+
+# Metadata
+
+
+
 
