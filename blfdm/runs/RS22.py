427bb89f3e32f447934022795be7a297078b8185
Mark Schlutow <mschlutow@bgc-jena.mpg.de>
2025-03-26 09:54:35 +0100

    Bug in solver discovered. Shifting incorrect.

--- blfdm/runs/RS22.py
+++ blfdm/runs/RS22.py
@@ -8,12 +8,12 @@ from ..src.utils import point_source
 from ..src.utils import compute_wind_fields
 from ..src.solver import steady_state_transport_solver
 
-nxy         = 512, 512
+nxy         = 512, 256
 nz          = 10
 modes       = 512, 512
-domain      = 100.0, 100.0
-fetch       = 200.0
-meas_pt     = 50.0, 50.0
+domain      = 100.0, 50.0
+fetch       = 400.0
+meas_pt     = 50.0, 20.0
 meas_height = 3.9
 start_doy   = 228
 end_doy     = 230
@@ -22,7 +22,7 @@ end_hour    = 21.0
 ustar_min   = 0.2
 
 dist = 20.0  # [m] distance of release pipe from tower
-azi  = 253.0 # [deg] azimuth of release pipe
+azi  = 253.0 # [deg] azimuth of release pipe measured from tower from North
 vfr  = 1.75  # [L min-1] volumetric flow rate at release pipe
 
 # df = pd.read_csv(file_path, na_values='<missing>')
@@ -77,15 +77,16 @@ surf_flx[iy,ix] = 1.0
 plt.imshow(surf_flx, origin="lower", extent=[0,domain[0],0,domain[1]])
 plt.plot(meas_pt[0], meas_pt[1],"ro")
 # plt.plot(src_pt[0], src_pt[1],"go")
+plt.colorbar()
 plt.show()
 
 flx_mean = np.zeros(nxy)
 flxm = np.zeros(nt)
 
-for n in range(nt):
+for n in range(92,93):
  
     print("n    =",n)
-    print("DOY =",doy[n])
+    print("DOY  =",doy[n])
     print("time =",time[n])
     print()
  
@@ -97,10 +98,11 @@ for n in range(nt):
          wind,
          ustar[n],
          mol[n],
+         # z0 = 0.002,
          z0_min = 0.001,
          z0_max = 0.100)
  
-    p0, p00, p, flx = steady_state_transport_solver(
+    conc0, conc00, conc, flx = steady_state_transport_solver(
          surf_flx,
          z, 
          profs, 
@@ -112,15 +114,23 @@ for n in range(nt):
     plt.imshow(flx, origin="lower", extent=[0,domain[0],0,domain[1]])
     plt.plot(meas_pt[0], meas_pt[1],"ro")
     plt.plot(src_pt[0], src_pt[1],"go")
+    plt.colorbar()
+    plt.show()
+
+    plt.imshow(conc, origin="lower", extent=[0,domain[0],0,domain[1]])
+    plt.plot(meas_pt[0], meas_pt[1],"ro")
+    plt.plot(src_pt[0], src_pt[1],"go")
+    plt.colorbar()
     plt.show()
 
     # taking the measurement
-    flxm[n] = flx[nxy[1]//2,nxy[0]//2]
+    flxm[n] = flx[nxy[1]//2, nxy[0]//2]
     flx_mean = flx_mean + flx
     print("flxm =",flxm[n])
     print()
 
 plt.plot(flxo,flxm,'.')
+plt.loglog()
 plt.show()
 
 # plt.imshow(qmean, origin="lower", extent=[0,domain[0],0,domain[1]])
