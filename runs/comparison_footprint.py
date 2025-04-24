1db5bac88908ba3b4d11731b5684311da21e4c4e
ray-chew <chew@caltech.edu>
2025-04-24 11:18:26 -0700

    applied black formatting throughout

--- runs/comparison_footprint.py
+++ runs/comparison_footprint.py
@@ -7,77 +7,71 @@ from ..src.solver import steady_state_transport_solver
 from ..src.calc_footprint_FFP import FFP
 from ..src.ffm_kormann_meixner import estimateFootprint as FKM
 
-nxy         = 512, 512
-nz          = 128
-modes       = 1024, 1024
-#modes       = 128, 128
-domain      = 200.0, 600.0
-fetch       = 1000.0
-meas_pt     = 100.0, 0.0
+nxy = 512, 512
+nz = 128
+modes = 1024, 1024
+# modes       = 128, 128
+domain = 200.0, 600.0
+fetch = 1000.0
+meas_pt = 100.0, 0.0
 meas_height = 10.0
-wind        = 0.0, -1.0
-ustar       = 0.4
-z0          = 0.1
-mol         = -20.0
+wind = 0.0, -1.0
+ustar = 0.4
+z0 = 0.1
+mol = -20.0
 
-wd = np.atan(wind[0]/wind[1]) * 180.0 / np.pi
-umean = np.sqrt(wind[0]**2+wind[1]**2)
+wd = np.atan(wind[0] / wind[1]) * 180.0 / np.pi
+umean = np.sqrt(wind[0] ** 2 + wind[1] ** 2)
 
 ############################################################
-###  BLDFM 
+###  BLDFM
 ############################################################
 
 surf_flx = ideal_source(nxy, domain)
 
-z, profs = vertical_profiles(
-        nz, 
-        meas_height, 
-        wind,
-        ustar,
-        z0 = z0,
-        mol = mol)
+z, profs = vertical_profiles(nz, meas_height, wind, ustar, z0=z0, mol=mol)
 
 srf_flx, bg_conc, conc, flx = steady_state_transport_solver(
-        surf_flx, 
-        z, 
-        profs, 
-        domain,
-        modes = modes,
-        meas_pt = meas_pt,
-        footprint = True,
-        fetch = fetch
-        )
+    surf_flx,
+    z,
+    profs,
+    domain,
+    modes=modes,
+    meas_pt=meas_pt,
+    footprint=True,
+    fetch=fetch,
+)
 
 ############################################################
 ###  Kljun's FFP footprint model
 ############################################################
 
 #
-#FFP_res = FFP(
-#        zm = meas_height, 
-#        z0 = z0, 
+# FFP_res = FFP(
+#        zm = meas_height,
+#        z0 = z0,
 #        umean = umean,
-#        h = 2000., 
-#        ol = mol, 
-#        sigmav = 0.2, 
-#        ustar = 0.437, 
+#        h = 2000.,
+#        ol = mol,
+#        sigmav = 0.2,
+#        ustar = 0.437,
 #        wind_dir = 180.0,
 #        nx = 1000,
 #        rs= [20., 40., 60., 80.],
 #        fig = False,
 #        crop = 0)
 #
-#msk  = (FFP_res["y_2d"] <= domain[0]/2) & \
+# msk  = (FFP_res["y_2d"] <= domain[0]/2) & \
 #       (FFP_res["y_2d"] >= -domain[0]/2) & \
 #       (FFP_res["x_2d"] >=  0.0) & \
 #       (FFP_res["x_2d"] <=  domain[1])
 #
-#f_2d = np.ma.masked_array(FFP_res["f_2d"], msk)
+# f_2d = np.ma.masked_array(FFP_res["f_2d"], msk)
 ##f_2d = FFP_res["f_2d"][msk]
 ##x_2d = FFP_res["x_2d"][msk]
 ##y_2d = FFP_res["y_2d"][msk]
 #
-#print(f_2d.shape)
+# print(f_2d.shape)
 ##print(x_2d.shape)
 ##print(y_2d.shape)
 
@@ -87,16 +81,17 @@ srf_flx, bg_conc, conc, flx = steady_state_transport_solver(
 ############################################################
 
 grid_x, grid_y, grid_ffm = FKM(
-        zm = meas_height, 
-        z0 = z0, 
-        ws = umean, 
-        ustar = ustar, 
-        mo_len = mol, 
-        sigma_v = 0.5*ustar,
-        grid_domain = [0,domain[0],0,domain[1]], 
-        grid_res = domain[0] / nxy[0], 
-        mxy = meas_pt, 
-        wd = wd)
+    zm=meas_height,
+    z0=z0,
+    ws=umean,
+    ustar=ustar,
+    mo_len=mol,
+    sigma_v=0.5 * ustar,
+    grid_domain=[0, domain[0], 0, domain[1]],
+    grid_res=domain[0] / nxy[0],
+    mxy=meas_pt,
+    wd=wd,
+)
 
 
 ############################################################
@@ -106,48 +101,39 @@ grid_x, grid_y, grid_ffm = FKM(
 shrink = 0.5
 cmap = "turbo"
 
-fig, axs = plt.subplots(
-        1, 2, 
-        figsize=[6, 8], 
-        sharey=True, 
-        layout='constrained')
+fig, axs = plt.subplots(1, 2, figsize=[6, 8], sharey=True, layout="constrained")
 
 # BLDFM
 plot = axs[0].imshow(
-        flx, 
-        origin="lower", 
-        cmap=cmap, 
-        extent=[0,domain[0],0,domain[1]])
+    flx, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+)
 
 axs[0].set_title("BLDFM")
 axs[0].set_xlabel("x [m]")
 axs[0].set_ylabel("y [m]")
 
 # FFP
-#plot = axs[1].pcolormesh(x_2d, y_2d, f_2d)
-#plot = axs[1].imshow(
-#        f_2d, 
-#        origin="lower", 
-#        cmap=cmap, 
+# plot = axs[1].pcolormesh(x_2d, y_2d, f_2d)
+# plot = axs[1].imshow(
+#        f_2d,
+#        origin="lower",
+#        cmap=cmap,
 #        extent=[0,domain[0],0,domain[1]])
 #
-#axs[1].set_title("FFP")
-#axs[1].set_xlabel("x [m]")
+# axs[1].set_title("FFP")
+# axs[1].set_xlabel("x [m]")
 
 # FKM
 plot = axs[1].imshow(
-        grid_ffm, 
-        origin="upper", 
-        cmap=cmap, 
-        extent=[0,domain[0],0,domain[1]])
+    grid_ffm, origin="upper", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+)
 
 axs[1].set_title("FKM")
 axs[1].set_xlabel("x [m]")
-cbar = fig.colorbar(plot, ax=axs, shrink=shrink, location='bottom')
+cbar = fig.colorbar(plot, ax=axs, shrink=shrink, location="bottom")
 cbar.formatter.set_powerlimits((0, 0))
 cbar.formatter.set_useMathText(True)
 cbar.set_label("$m^{-2}$")
 
-#plt.show()
+# plt.show()
 plt.savefig("comparison_footprint.png", dpi=300)
-
--- src/ffm_kormann_meixner.py
+++ src/ffm_kormann_meixner.py
@@ -1,7 +1,7 @@
 # Kormann & Meixner footprint model (Kormann and Meixner, 2001). The
 # implementation is based on a MATLAB script originally by Jakob Sievers
 # (05/2013) but revised and annotated by Christian Wille.
-# 
+#
 # Zhan Li, zhanli@gfz-potsdam.de
 # Created: Sat Aug 22 18:41:23 CEST 2020
 
@@ -12,12 +12,13 @@ from scipy import special as spsp
 
 von_karman = 0.4
 
+
 def estimateZ0(zm, ws, wd, ustar, mo_len, half_wd_win=22):
-    """Estimate roughness lengths based on Kormann-Meixner footprint model. 
+    """Estimate roughness lengths based on Kormann-Meixner footprint model.
 
     Estimate roughness length (z0) by relating power-law wind profile in
     Kormann & Meixner's footprint model (Kormann and Meixner, 2001) to log wind
-    profile in Monin-Obukhov similarity theory. 
+    profile in Monin-Obukhov similarity theory.
 
     The number of input observations at multiple time steps should be large
     enough to cover all possible wind directions because this function outputs
@@ -43,7 +44,7 @@ def estimateZ0(zm, ws, wd, ustar, mo_len, half_wd_win=22):
         List of friction velocity (m*s^-1) per observation at one time step.
 
     mo_len : ndarray of shape (n_obs,)
-        List of Monin-Obukhov length (meter) per observation at one time step. 
+        List of Monin-Obukhov length (meter) per observation at one time step.
 
     half_wd_win : float
         Half window size of wind directions to smooth estimates of roughness
@@ -68,10 +69,12 @@ def estimateZ0(zm, ws, wd, ustar, mo_len, half_wd_win=22):
 
     n_obs = len(zm)
     # Check if inputs are of the same length
-    if (n_obs != len(ws) \
-            or n_obs != len(wd) \
-            or n_obs != len(ustar) \
-            or n_obs != len(mo_len)):
+    if (
+        n_obs != len(ws)
+        or n_obs != len(wd)
+        or n_obs != len(ustar)
+        or n_obs != len(mo_len)
+    ):
         raise RuntimeError("Input parameters must be of the same length!")
 
     psi_m = _psiM(zm, mo_len)
@@ -80,7 +83,7 @@ def estimateZ0(zm, ws, wd, ustar, mo_len, half_wd_win=22):
     z0 = zm * np.exp(psi_m - (k * ws / ustar))
 
     # remove outliers
-    z0[z0>1000] = np.nan;
+    z0[z0 > 1000] = np.nan
 
     # NOTE: here z0, raw values of roughness length from solving Eq. (31) in
     # (Kormann and Meixner, 2001) is smoothed using median in a 45-deg window
@@ -88,28 +91,32 @@ def estimateZ0(zm, ws, wd, ustar, mo_len, half_wd_win=22):
     # observations matter.
     if half_wd_win < 1:
         return z0
-    
-    z0med = np.zeros_like(z0) + np.nan;
+
+    z0med = np.zeros_like(z0) + np.nan
     for kk in range(0, 360):
-        wd_wrapped = wd.copy();
-        if kk<90:
-            wd_wrapped[wd>270] = wd[wd>270] - 360
-        elif kk>270:
-            wd_wrapped[wd<90] = wd[wd<90] + 360
-        idx1 = np.logical_and(wd >= kk, wd < (kk+1))        
-        idx2 = np.logical_and(wd_wrapped >= (kk-half_wd_win), wd_wrapped < (kk+1+half_wd_win))
+        wd_wrapped = wd.copy()
+        if kk < 90:
+            wd_wrapped[wd > 270] = wd[wd > 270] - 360
+        elif kk > 270:
+            wd_wrapped[wd < 90] = wd[wd < 90] + 360
+        idx1 = np.logical_and(wd >= kk, wd < (kk + 1))
+        idx2 = np.logical_and(
+            wd_wrapped >= (kk - half_wd_win), wd_wrapped < (kk + 1 + half_wd_win)
+        )
         z0med[idx1] = np.nanmedian(z0[idx2])
     return z0med
 
-def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
-        grid_domain, grid_res, mxy, wd=None):
+
+def estimateFootprint(
+    zm, z0, ws, ustar, mo_len, sigma_v, grid_domain, grid_res, mxy, wd=None
+):
     """Estimate footprint of one measurement at one time step using Kormann &
     Meixner model.
 
     The estimation of the footprint of one measurement at one time step here
     uses the analytical approach in Kormann & Meixner's footprint model
     (Kormann and Meixner, 2001) to estimate two parameters, m and n for the
-    power-law wind profile used by this footprint model.  
+    power-law wind profile used by this footprint model.
 
     The implementation is based on a MATLAB script originally by Jakob Sievers
     (05/2013) but revised and annotated by Christian Wille.
@@ -117,7 +124,7 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
     Parameters
     ----------
     zm : float
-        The measurement/receptor height (meter) above zero displacement plane. 
+        The measurement/receptor height (meter) above zero displacement plane.
 
     z0 : float
         The roughness length (meter).
@@ -125,11 +132,11 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
     ws : float
         The wind speed (m*s^-1).
 
-    ustar : float 
+    ustar : float
         The friction velocity (m*s^-1).
 
     mo_len : float
-        The Monin-Obukhov length (meter). 
+        The Monin-Obukhov length (meter).
 
     sigma_v : float
         The standard deviation of cross-wind (m*s^-1).
@@ -142,34 +149,34 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
         The `[xmin, ymax]` will be the upper left corner of the upper left cell
         in the output grid.  The X and Y directions of this coordinate system
         are set up in one the following two ways.
-        
+
         If wind direction is given by the optional parameter `wd`, the grid will
         be set up to align with the X-Y axes where wind direction is measured
         (clockwise from positive Y, that is, similar to how azimuth angle is
         measured). For example, if wind direction is measured with regard to
         true/due north. Then the output grid will align with the X axis from
-        west towards east and the Y axis from south towards north. 
+        west towards east and the Y axis from south towards north.
 
         If no wind direction is given, the output grid aligns with along-wind
         and cross-wind direction.  Specifically in this coordinate system, the
         X axis points to the wind direction angle (where wind comes from, i.e.,
         positive X indicates upwind distances), then the Y axis is defined as
         perpendicular to the wind direction (i.e., cross-wind direction) and
-        with the Y axis direction chosen to produce right-handed coordinates. 
-        
+        with the Y axis direction chosen to produce right-handed coordinates.
+
     grid_res : float
         The resolution (meter) of the grid on which the footprint will be
-        estimated. 
+        estimated.
 
     mxy : array-like of shape (2,)
         The [x, y] of the measurement/receptor in the coordinate system of
-        `grid_domain`. 
+        `grid_domain`.
 
     wd : float
         The wind direction (degree), an angle where wind comes from. It is
         measured clockwise with regard to the Y axis of a cardinal coordinate
         system. The output grid of flux footprint will align with the axes of
-        this coordinate system. 
+        this coordinate system.
 
         If you want the output grid to align with a pre-defined raster grid,
         you have to provide an adjusted wind direction angle that is with
@@ -179,25 +186,25 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
         usually not the same in many map projections where raster grids are
         defined. The difference between true north and grid north is called
         "meridian convergence" of a map projection. It depends on geolocation
-        and map projection. 
+        and map projection.
 
     Returns
     -------
     grid_x : ndarray of shape (grid_ysize, grid_xsize)
         The X coordinates of cell centers of the grid on which flux footprint
         is estimated. The `grid_ysize` and `grid_xsize` are the number of cells
-        along X and Y axes, that is, columns and rows. 
+        along X and Y axes, that is, columns and rows.
 
     grid_y : ndarray of shape (grid_ysize, grid_xsize)
         The Y coordinates of cell centers of the grid on which flux footprint
         is estimated. The `grid_ysize` and `grid_xsize` are the number of cells
-        along X and Y axes, that is, columns and rows. 
+        along X and Y axes, that is, columns and rows.
 
     grid_ffm : ndarray of shape (grid_ysize, grid_xsize)
         The flux footprint, phi(x, y, z) in (Kormann and Meixner, 2001) that
         describes the flux portion (m^-2) seen by the measurement/receptor. The
         `grid_ysize` and `grid_xsize` are the number of cells along X and Y
-        axes, that is, columns and rows. 
+        axes, that is, columns and rows.
 
     Refernces
     ---------
@@ -210,10 +217,12 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
 
     # set up the output grid
     xmin, xmax, ymin, ymax = tuple(grid_domain)
-    grid_x, grid_y = np.meshgrid(np.arange(xmin+0.5*grid_res, xmax, grid_res), \
-            np.arange(ymax-0.5*grid_res, ymin, -grid_res))
+    grid_x, grid_y = np.meshgrid(
+        np.arange(xmin + 0.5 * grid_res, xmax, grid_res),
+        np.arange(ymax - 0.5 * grid_res, ymin, -grid_res),
+    )
     grid_ffm = np.zeros_like(grid_x)
-    
+
     # phi_m, Eq. (33) in (Kormann and Meixner, 2001), stability function
     phi_m = _phiM(np.asarray([zm]), np.asarray([mo_len]))[0]
     # phi_c, Eq. (34) in (Kormann and Meixner, 2001), stability function
@@ -222,7 +231,9 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
     # the wind profile.
     psi_m = _psiM(np.asarray([zm]), np.asarray([mo_len]))[0]
 
-    m = _mParam(np.asarray([zm]), np.asarray([ws]), np.asarray([ustar]), np.asarray([mo_len]))[0]
+    m = _mParam(
+        np.asarray([zm]), np.asarray([ws]), np.asarray([ustar]), np.asarray([mo_len])
+    )[0]
     n = _nParam(np.asarray([zm]), np.asarray([mo_len]))[0]
 
     # kappa, from solving Eq. (11) & (32) in (Kormann and Meixner, 2001),
@@ -235,13 +246,18 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
     U = ustar * (np.log(zm / z0) + psi_m) / (k * zm**m)
     if U < 0:
         # Physically impossible
-        msg = 'U in Eq. (11) of Kormann & Meixner, 2001 is estimated ' \
-                + 'negative as {0:.3f}, physically impossible! ' \
-                + 'Return empty footprint.'
+        msg = (
+            "U in Eq. (11) of Kormann & Meixner, 2001 is estimated "
+            + "negative as {0:.3f}, physically impossible! "
+            + "Return empty footprint."
+        )
         msg = msg.format(U)
-        msg = msg + '\n' \
-                + 'zm = {0:.3f}, z0 = {1:.3f}, ws = {2:.3f}, ' \
-                + 'ustar = {3:.3f}, L = {4:.3f}, sigma_v = {5:.3f}'
+        msg = (
+            msg
+            + "\n"
+            + "zm = {0:.3f}, z0 = {1:.3f}, ws = {2:.3f}, "
+            + "ustar = {3:.3f}, L = {4:.3f}, sigma_v = {5:.3f}"
+        )
         msg = msg.format(zm, z0, ws, ustar, mo_len, sigma_v)
         warnings.warn(msg)
         return grid_x, grid_y, grid_ffm
@@ -251,24 +267,24 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
     # mu, p.213 in (Kormann and Meixner, 2001), constant
     mu = (1 + m) / r
     # Xi, Eq. 19 in (Kormann and Meixner, 2001), flux length scale
-    Xi = U * zm**r / (r**2 * kappa);
+    Xi = U * zm**r / (r**2 * kappa)
 
     # aggregate variables for use in the final footprint equation (combination
     # of Eq. (9) and (21) in (Kormann and Meixner, 2001)).
 
-    # gamma, Eq. (18) in (Kormann and Meixner, 2001). 
-    gmm = spsp.gamma(mu);
-    mr = m / r;
-    
+    # gamma, Eq. (18) in (Kormann and Meixner, 2001).
+    gmm = spsp.gamma(mu)
+    mr = m / r
+
     # A, a partial term of 1/sigma after combining Eq. (18) and p.212 (lines
     # after Eq. (9)) in (Kormann and Meixner, 2001)
-    A = U / (spsp.gamma(1 / r) * sigma_v) * (kappa * r**2 / U)**mr;
-    
+    A = U / (spsp.gamma(1 / r) * sigma_v) * (kappa * r**2 / U) ** mr
+
     # num, a partial term of footprint phi(x,y,z) after combining Eq. (9) and
     # (21) in (Kormann and Meixner, 2001). By combining these two equations, we
     # will cancel a pair of gamma(mu) and hence reduce some computational
     # errors in the final footprint calculation.
-    num = (1 / np.sqrt(2 * np.pi)) * Xi**mu;
+    num = (1 / np.sqrt(2 * np.pi)) * Xi**mu
 
     if wd is None:
         # No wind direction given, the footprint grid aligns with along-wind
@@ -285,28 +301,34 @@ def estimateFootprint(zm, z0, ws, ustar, mo_len, sigma_v, \
         # cross-wind directions for their use in the footprint function.
         #
         # First shift (0, 0) to measurement/receptor location and then rotate
-        # coordinates in a polar coordinate system for simplicity. 
+        # coordinates in a polar coordinate system for simplicity.
         x, y = grid_x - mxy[0], grid_y - mxy[1]
         rho = np.sqrt(x**2 + y**2)
         # numpy's arctan2 function defines angular coordinates theta
         # counter-clockwise from X positive. Pay extra attention here because
         # this is different from the grid coordinate system where wind
-        # direction angle is defined, clockwise from Y positive. 
+        # direction angle is defined, clockwise from Y positive.
         theta = np.arctan2(y, x)
-        new_theta = theta + np.deg2rad(wd) - np.pi*0.5
+        new_theta = theta + np.deg2rad(wd) - np.pi * 0.5
         x = rho * np.cos(new_theta)
         y = rho * np.sin(new_theta)
 
     # phi, footprint function, flux portion at (x, y) location seen by the
     # measurement/receptor, by combining the Eq. (9) and (21) in (Kormann and
-    # Meixner, 2001). 
-    sflag = x > 0 # Only upwind contributes to flux measurements.
-    grid_ffm[sflag] = grid_res**2 \
-            * num * A * x[sflag]**(mr - 2 - mu) \
-            * np.exp(-Xi / x[sflag] \
-                     -0.5 * (gmm * y[sflag] * A * x[sflag]**(mr-1))**2)
+    # Meixner, 2001).
+    sflag = x > 0  # Only upwind contributes to flux measurements.
+    grid_ffm[sflag] = (
+        grid_res**2
+        * num
+        * A
+        * x[sflag] ** (mr - 2 - mu)
+        * np.exp(
+            -Xi / x[sflag] - 0.5 * (gmm * y[sflag] * A * x[sflag] ** (mr - 1)) ** 2
+        )
+    )
 
     return grid_x, grid_y, grid_ffm
+
 
 def _phiM(zm, mo_len):
     """Calculate phi_m using Eq. (33), the stability function in (Kormann and
@@ -315,10 +337,10 @@ def _phiM(zm, mo_len):
     Parameters
     ----------
     zm : array-like of shape (n_obs,)
-        List of measurement height (meter) per observation at one time steps. 
+        List of measurement height (meter) per observation at one time steps.
 
     mo_len : arry-like of shape (n_obs,)
-        List of Monin-Obukhov length (meter) per observation at one time step. 
+        List of Monin-Obukhov length (meter) per observation at one time step.
 
     Returns
     -------
@@ -327,10 +349,11 @@ def _phiM(zm, mo_len):
     """
     phi_m = np.zeros_like(zm)
     sflag = mo_len < 0
-    phi_m[sflag] = (1 - 16 * zm[sflag] / mo_len[sflag])**(-0.25)
+    phi_m[sflag] = (1 - 16 * zm[sflag] / mo_len[sflag]) ** (-0.25)
     sflag = mo_len >= 0
     phi_m[sflag] = 1 + 5 * zm[sflag] / mo_len[sflag]
     return phi_m
+
 
 def _phiC(zm, mo_len):
     """Calculate phi_c using Eq. (34), the stability function in (Kormann and
@@ -339,10 +362,10 @@ def _phiC(zm, mo_len):
     Parameters
     ----------
     zm : array-like of shape (n_obs,)
-        List of measurement height (meter) per observation at one time steps. 
+        List of measurement height (meter) per observation at one time steps.
 
     mo_len : arry-like of shape (n_obs,)
-        List of Monin-Obukhov length (meter) per observation at one time step. 
+        List of Monin-Obukhov length (meter) per observation at one time step.
 
     Returns
     -------
@@ -351,10 +374,11 @@ def _phiC(zm, mo_len):
     """
     phi_c = np.zeros_like(zm)
     sflag = mo_len < 0
-    phi_c[sflag] = (1 - 16 * zm[sflag] / mo_len[sflag])**(-0.5)
+    phi_c[sflag] = (1 - 16 * zm[sflag] / mo_len[sflag]) ** (-0.5)
     sflag = mo_len >= 0
     phi_c[sflag] = 1 + 5 * zm[sflag] / mo_len[sflag]
     return phi_c
+
 
 def _psiM(zm, mo_len):
     """Calculate psi_m using Eq. (35), the diabatic integration of the wind
@@ -363,10 +387,10 @@ def _psiM(zm, mo_len):
     Parameters
     ----------
     zm : array-like of shape (n_obs,)
-        List of measurement height (meter) per observation at one time steps. 
+        List of measurement height (meter) per observation at one time steps.
 
     mo_len : arry-like of shape (n_obs,)
-        List of Monin-Obukhov length (meter) per observation at one time step. 
+        List of Monin-Obukhov length (meter) per observation at one time step.
 
     Returns
     -------
@@ -375,13 +399,17 @@ def _psiM(zm, mo_len):
     """
     psi_m = np.zeros_like(zm)
     sflag = mo_len < 0
-    inv_phi_m = (1 - 16 * zm[sflag] / mo_len[sflag])**(0.25)
-    psi_m[sflag] = -2 * np.log(0.5 * (1 + inv_phi_m)) \
-            - np.log(0.5 * (1 + inv_phi_m**2)) \
-            + 2 * np.arctan(inv_phi_m) - np.pi * 0.5
+    inv_phi_m = (1 - 16 * zm[sflag] / mo_len[sflag]) ** (0.25)
+    psi_m[sflag] = (
+        -2 * np.log(0.5 * (1 + inv_phi_m))
+        - np.log(0.5 * (1 + inv_phi_m**2))
+        + 2 * np.arctan(inv_phi_m)
+        - np.pi * 0.5
+    )
     sflag = mo_len >= 0
     psi_m[sflag] = 5 * zm[sflag] / mo_len[sflag]
     return psi_m
+
 
 def _mParam(zm, ws, ustar, mo_len):
     """Estimate the parameter m, in Eq. (11) of (Kormann and Meixner, 2001),
@@ -391,7 +419,7 @@ def _mParam(zm, ws, ustar, mo_len):
     Parameters
     ----------
     zm : array-like of shape (n_obs,)
-        List of measurement height (meter) per observation at one time steps. 
+        List of measurement height (meter) per observation at one time steps.
 
     ws : array-like of shape (n_obs,)
         List of wind speed (m*s^-1) per observation at one time step.
@@ -400,7 +428,7 @@ def _mParam(zm, ws, ustar, mo_len):
         List of friction velocity (m*s^-1) per observation at one time step.
 
     mo_len : array-like of shape (n_obs,)
-        List of Monin-Obukhov length (meter) per observation at one time step. 
+        List of Monin-Obukhov length (meter) per observation at one time step.
 
     Returns
     -------
@@ -410,10 +438,11 @@ def _mParam(zm, ws, ustar, mo_len):
     k = von_karman
 
     # Estimate m using the analytical approach in (Kormann and Meixner, 2001),
-    # following the Eq. (36). 
+    # following the Eq. (36).
     phi_m = _phiM(zm, mo_len)
     m = ustar * phi_m / (k * ws)
     return m
+
 
 def _nParam(zm, mo_len):
     """Estimate the parameter n, in Eq. (11) of (Kormann and Meixner, 2001),
@@ -423,10 +452,10 @@ def _nParam(zm, mo_len):
     Parameters
     ----------
     zm : array-like of shape (n_obs,)
-        List of measurement height (meter) per observation at one time steps. 
+        List of measurement height (meter) per observation at one time steps.
 
     mo_len : array-like of shape (n_obs,)
-        List of Monin-Obukhov length (meter) per observation at one time step. 
+        List of Monin-Obukhov length (meter) per observation at one time step.
 
     Returns
     -------
@@ -437,8 +466,9 @@ def _nParam(zm, mo_len):
     # following the Eq. (36).
     n = np.zeros_like(zm)
     sflag = mo_len < 0
-    n[sflag] = (1 - 24 * zm[sflag] / mo_len[sflag]) \
-            / (1 - 16 * zm[sflag] / mo_len[sflag])
+    n[sflag] = (1 - 24 * zm[sflag] / mo_len[sflag]) / (
+        1 - 16 * zm[sflag] / mo_len[sflag]
+    )
     sflag = mo_len >= 0
     n[sflag] = 1 / (1 + 5 * zm[sflag] / mo_len[sflag])
     return n
