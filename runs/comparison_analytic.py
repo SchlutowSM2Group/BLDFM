194f634017c04208ca9a38fcc2ea5400e30ff769
ray-chew <chew@caltech.edu>
2025-04-24 11:18:26 -0700

    applied black formatting throughout

--- runs/comparison_analytic.py
+++ runs/comparison_analytic.py
@@ -5,123 +5,97 @@ from ..src.pbl_model import vertical_profiles
 from ..src.utils import point_source
 from ..src.solver import steady_state_transport_solver
 
-nxy         = 512, 256
-modes       = 1024, 1024
-nz          = 512
-#nxy         = 128, 64
-#modes       = 128, 128
-#nz          = 16
-domain      = 200.0, 100.0
-src_pt      = 10.0, 10.0 
-fetch       = 1000.0
+nxy = 512, 256
+modes = 1024, 1024
+nz = 512
+# nxy         = 128, 64
+# modes       = 128, 128
+# nz          = 16
+domain = 200.0, 100.0
+src_pt = 10.0, 10.0
+fetch = 1000.0
 meas_height = 10.0
-wind        = 4.0, 1.0
-ustar       = 0.4
+wind = 4.0, 1.0
+ustar = 0.4
 
 srf_flx = point_source(nxy, domain, src_pt)
 
-z, profs = vertical_profiles(
-        nz, 
-        meas_height, 
-        wind,
-        ustar,
-        closure = "CONSTANT")
+z, profs = vertical_profiles(nz, meas_height, wind, ustar, closure="CONSTANT")
 
 srf_conc_ana, bg_conc_ana, conc_ana, flx_ana = steady_state_transport_solver(
-        srf_flx, 
-        z, 
-        profs, 
-        domain,
-        modes = modes,
-        fetch = fetch,
-        analytic = True)
+    srf_flx, z, profs, domain, modes=modes, fetch=fetch, analytic=True
+)
 
 srf_conc, bg_conc, conc, flx = steady_state_transport_solver(
-        srf_flx, 
-        z, 
-        profs, 
-        domain,
-        modes = modes,
-        fetch = fetch,
-        ivp_method = "TSEI3")
+    srf_flx, z, profs, domain, modes=modes, fetch=fetch, ivp_method="TSEI3"
+)
 
-#diff_conc = (conc - conc_ana) / np.mean(conc_ana)
-#diff_flx  = (flx - flx_ana) / np.mean(flx_ana)
+# diff_conc = (conc - conc_ana) / np.mean(conc_ana)
+# diff_flx  = (flx - flx_ana) / np.mean(flx_ana)
 
 diff_conc = (conc - conc_ana) / np.max(conc_ana)
-diff_flx  = (flx - flx_ana) / np.max(flx_ana)
+diff_flx = (flx - flx_ana) / np.max(flx_ana)
 
 shrink = 0.7
 cmap = "turbo"
 
 fig, axs = plt.subplots(
-        2,2, 
-        figsize=[10, 6], 
-        sharex=True, 
-        sharey=True, 
-        layout='constrained')
+    2, 2, figsize=[10, 6], sharex=True, sharey=True, layout="constrained"
+)
 
-plot = axs[0,0].imshow(
-        conc, 
-        origin="lower", 
-        cmap=cmap, 
-        extent=[0,domain[0],0,domain[1]])
+plot = axs[0, 0].imshow(
+    conc, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+)
 
-axs[0,0].plot(src_pt[0],src_pt[1],'ro')
-axs[0,0].set_title("Numerical concentration")
-axs[0,0].set_ylabel("y [m]")
-axs[0,0].xaxis.set_tick_params(labelbottom=False)
-cbar = fig.colorbar(plot, ax=axs[0,0], shrink=shrink, location='bottom')
+axs[0, 0].plot(src_pt[0], src_pt[1], "ro")
+axs[0, 0].set_title("Numerical concentration")
+axs[0, 0].set_ylabel("y [m]")
+axs[0, 0].xaxis.set_tick_params(labelbottom=False)
+cbar = fig.colorbar(plot, ax=axs[0, 0], shrink=shrink, location="bottom")
 cbar.formatter.set_powerlimits((0, 0))
 cbar.formatter.set_useMathText(True)
 cbar.set_label("a.u.")
 
-plot = axs[0,1].imshow(
-        flx, 
-        origin="lower", 
-        cmap=cmap, 
-        extent=[0,domain[0],0,domain[1]])
+plot = axs[0, 1].imshow(
+    flx, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+)
 
-axs[0,1].plot(src_pt[0],src_pt[1],'ro')
-axs[0,1].set_title("Numerical flux")
-axs[0,1].xaxis.set_tick_params(labelbottom=False)
-axs[0,1].yaxis.set_tick_params(labelleft=False)
-cbar = fig.colorbar(plot, ax=axs[0,1], shrink=shrink, location='bottom')
+axs[0, 1].plot(src_pt[0], src_pt[1], "ro")
+axs[0, 1].set_title("Numerical flux")
+axs[0, 1].xaxis.set_tick_params(labelbottom=False)
+axs[0, 1].yaxis.set_tick_params(labelleft=False)
+cbar = fig.colorbar(plot, ax=axs[0, 1], shrink=shrink, location="bottom")
 cbar.formatter.set_powerlimits((0, 0))
 cbar.formatter.set_useMathText(True)
 cbar.set_label("a.u. m/s")
 
 cmap = "turbo"
 
-plot = axs[1,0].imshow(
-        diff_conc, 
-        origin="lower", 
-        cmap=cmap, 
-        extent=[0,domain[0],0,domain[1]])
+plot = axs[1, 0].imshow(
+    diff_conc, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+)
 
-#axs[1,0].plot(src_pt[0],src_pt[1],'ro')
-axs[1,0].set_title("Relative difference to analytic concentration")
-axs[1,0].set_xlabel("x [m]")
-axs[1,0].set_ylabel("y [m]")
-cbar = fig.colorbar(plot, ax=axs[1,0], shrink=shrink, location='bottom')
-#cbar.set_label('‰')
+# axs[1,0].plot(src_pt[0],src_pt[1],'ro')
+axs[1, 0].set_title("Relative difference to analytic concentration")
+axs[1, 0].set_xlabel("x [m]")
+axs[1, 0].set_ylabel("y [m]")
+cbar = fig.colorbar(plot, ax=axs[1, 0], shrink=shrink, location="bottom")
+# cbar.set_label('‰')
 cbar.formatter.set_powerlimits((0, 0))
 cbar.formatter.set_useMathText(True)
 
-plot = axs[1,1].imshow(
-        diff_flx, 
-        origin="lower", 
-        cmap=cmap, 
-        extent=[0,domain[0],0,domain[1]])
+plot = axs[1, 1].imshow(
+    diff_flx, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+)
 
-#axs[1,1].plot(src_pt[0],src_pt[1],'ro')
-axs[1,1].set_title("Relative difference to analytic flux")
-axs[1,1].set_xlabel("x [m]")
-axs[1,1].yaxis.set_tick_params(labelleft=False)
-cbar = fig.colorbar(plot, ax=axs[1,1], shrink=shrink, location='bottom')
-#cbar.set_label('‰')
+# axs[1,1].plot(src_pt[0],src_pt[1],'ro')
+axs[1, 1].set_title("Relative difference to analytic flux")
+axs[1, 1].set_xlabel("x [m]")
+axs[1, 1].yaxis.set_tick_params(labelleft=False)
+cbar = fig.colorbar(plot, ax=axs[1, 1], shrink=shrink, location="bottom")
+# cbar.set_label('‰')
 cbar.formatter.set_powerlimits((0, 0))
 cbar.formatter.set_useMathText(True)
 
-#plt.show()
+# plt.show()
 plt.savefig("comparison_analytic.png", dpi=300)
--- src/pbl_model.py
+++ src/pbl_model.py
@@ -1,42 +1,43 @@
 import numpy as np
 
+
 def vertical_profiles(
-        n,
-        meas_height,
-        wind,
-        ustar,
-        mol = 1e9,
-        prsc = 1.0,
-        closure = "MOST",
-        z0 = -1e9,
-        z0_min = 0.001,
-        z0_max = 2.0
-        ):
-    """ 
-    Computes profiles of horizontal wind and eddy diffusivity 
+    n,
+    meas_height,
+    wind,
+    ustar,
+    mol=1e9,
+    prsc=1.0,
+    closure="MOST",
+    z0=-1e9,
+    z0_min=0.001,
+    z0_max=2.0,
+):
+    """
+    Computes profiles of horizontal wind and eddy diffusivity
     depending on the boundary layer's stratifictation
     from point measurements with, e.g., Eddy Covariance systems
-    according to Monin-Obukhov Similarity Theory (MOST) 
+    according to Monin-Obukhov Similarity Theory (MOST)
     (Kormann and Meixner, 2001, https://doi.org/10.1023/A:1018991015119).
-    
+
     Parameters:
         n: scalar(int)
             Number of vertical grid points
         meas_height: scalar(float)
-            Measurement height 
+            Measurement height
         wind: array(float)
-            List of zonal and meridional wind components u and v and 
+            List of zonal and meridional wind components u and v and
             the measurement height
         ustar: scalar(float)
             Friction velocity
         mol: scalar(float)
             Monin-Obukhov length
         prsc: scalar(float)
-            Prandtl or Schmidt number depending on the scalar 
+            Prandtl or Schmidt number depending on the scalar
             that is subject to atmospheric dispersion
         constant: scalar(bool)
             Switch for returning constant profiles
-            
+
     Returns:
         z: array(float)
             1D array of vertical grid points
@@ -57,16 +58,16 @@ def vertical_profiles(
     vm = vm[..., np.newaxis]
     ustar = ustar[..., np.newaxis]
 
-    kap = 0.4 # Karman constant
+    kap = 0.4  # Karman constant
 
     if closure == "CONSTANT":
 
         Km = kap * ustar * zm / prsc
 
-        #z0 = np.zeros_like(um)
-        #z = np.array([ np.linspace( z00, zm, n ) for z00 in z0 ]).squeeze() # not working
-        z = np.linspace( 0.0, zm, n )
-        
+        # z0 = np.zeros_like(um)
+        # z = np.array([ np.linspace( z00, zm, n ) for z00 in z0 ]).squeeze() # not working
+        z = np.linspace(0.0, zm, n)
+
         u = um * np.ones(n)
         v = vm * np.ones(n)
         K = Km * np.ones(n)
@@ -74,12 +75,12 @@ def vertical_profiles(
     if closure == "MOST":
 
         # absolute wind at zm
-        absum = np.sqrt( um**2 + vm**2 ) 
+        absum = np.sqrt(um**2 + vm**2)
 
         if z0 < 0.0:
 
             # roughness length
-            z0 = zm * np.exp( -kap * absum / ustar + psi( zm/mol ) )
+            z0 = zm * np.exp(-kap * absum / ustar + psi(zm / mol))
 
             # sanity checks
             z0 = min(z0, z0_max)
@@ -87,22 +88,22 @@ def vertical_profiles(
 
         else:
 
-            ustar = absum * kap / (np.log(zm/z0) + psi(zm/mol)) 
+            ustar = absum * kap / (np.log(zm / z0) + psi(zm / mol))
 
         # equidistant vertical grid
         # find a way to vectorize this properly
-        # z = np.array([ np.linspace( z00, zm, n ) for z00 in z0 ]).squeeze() # not working 
-        z = np.linspace( z0, zm, n ) 
+        # z = np.array([ np.linspace( z00, zm, n ) for z00 in z0 ]).squeeze() # not working
+        z = np.linspace(z0, zm, n)
 
-        absu = ustar / kap * ( np.log( z/z0 ) + psi( z/mol ) ) 
+        absu = ustar / kap * (np.log(z / z0) + psi(z / mol))
 
         u = um / absum * absu
         v = vm / absum * absu
 
-        K = kap * ustar * z / phi( z/mol ) / prsc
+        K = kap * ustar * z / phi(z / mol) / prsc
 
     # One-and-a-half order closure
-    # according to Schumann-Lilly closure (Schumann, 1991) 
+    # according to Schumann-Lilly closure (Schumann, 1991)
     if closure == "OAAHOC":
 
         cl = 0.845
@@ -115,13 +116,13 @@ def vertical_profiles(
             tke = 1.0
 
         # absolute wind at zm
-        absum = np.sqrt( um**2 + vm**2 ) 
+        absum = np.sqrt(um**2 + vm**2)
 
         # roughness length
         z0 = zm * np.exp(-cm * cl * absum * np.sqrt(tke) / ustar**2)
 
         # equidistant vertical grid
-        z = np.linspace( z0, zm, n )
+        z = np.linspace(z0, zm, n)
 
         absu = ustar**2 / cm / cl / np.sqrt(tke) * np.log(z / z0)
 
@@ -130,82 +131,80 @@ def vertical_profiles(
 
         K = ch * cl * z * np.sqrt(tke)
 
-
-    print('Stats from vertical_profiles')
-    print('z0    = %.3f m' % z[0])
-    print('ustar = %.3f m s-1' % ustar)
-    print('umax  = %.3f m s-1, vmax = %.3f m s-1, Kmax = %.3f m2 s-1' 
-          % (max(u), max(v), max(K)))
-    print('umin  = %.3f m s-1, vmin = %.3f m s-1, Kmin = %.3f m2 s-1' 
-          % (min(u), min(v), min(K)))
+    print("Stats from vertical_profiles")
+    print("z0    = %.3f m" % z[0])
+    print("ustar = %.3f m s-1" % ustar)
+    print(
+        "umax  = %.3f m s-1, vmax = %.3f m s-1, Kmax = %.3f m2 s-1"
+        % (max(u), max(v), max(K))
+    )
+    print(
+        "umin  = %.3f m s-1, vmin = %.3f m s-1, Kmin = %.3f m2 s-1"
+        % (min(u), min(v), min(K))
+    )
     print()
 
     return z.squeeze(), (u.squeeze(), v.squeeze(), K.squeeze())
 
 
 def psi(x):
-    '''
+    """
     Businger–Dyer relationship
     x =  z / mol
-    '''
-    xi = np.where(
-            x > 0.0, 
-            np.nan, 
-            np.power(1.0 - 16.0 * x, 0.25, dtype = complex).real)
+    """
+    xi = np.where(x > 0.0, np.nan, np.power(1.0 - 16.0 * x, 0.25, dtype=complex).real)
     return np.where(
-            x > 0.0, 
-            5.0 * x, 
-            -2.0 * np.log(0.5 * (1.0 + xi)) - np.log(0.5 * (1.0 + xi**2))
-            +2.0 * np.arctan(xi) - 0.5 * np.pi)
+        x > 0.0,
+        5.0 * x,
+        -2.0 * np.log(0.5 * (1.0 + xi))
+        - np.log(0.5 * (1.0 + xi**2))
+        + 2.0 * np.arctan(xi)
+        - 0.5 * np.pi,
+    )
+
 
 def phi(x):
-    '''
+    """
     Businger–Dyer relationship
-    x =  z / mol 
-    '''
+    x =  z / mol
+    """
     return np.where(
-            x > 0.0, 
-            1.0 + 5.0 * x, 
-            np.power(1.0 - 16.0 * x, -0.5, dtype = complex).real)
+        x > 0.0, 1.0 + 5.0 * x, np.power(1.0 - 16.0 * x, -0.5, dtype=complex).real
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-
     fig, axs = plt.subplots(2)
-    for mol in [-1000,-500,-400,-100,-30,30,100,400,500,1000]:
+    for mol in [-1000, -500, -400, -100, -30, 30, 100, 400, 500, 1000]:
         z, (u, v, K) = vertical_profiles(
-                n=100,
-                meas_height=5.0,
-                wind=(3.0, 1.0),
-                ustar=0.4,
-                closure = "MOST",
-                mol=mol)
-        axs[0].plot(u,z)
-        axs[0].set_xlabel('u [m/s]')
-        axs[0].set_ylabel('z [m]')
-        axs[0].set_title('Profiles for MOST')
-        axs[1].plot(K,z)
-        axs[1].set_xlabel('K [m2 s-1]')
-        axs[1].set_ylabel('z [m]')
+            n=100, meas_height=5.0, wind=(3.0, 1.0), ustar=0.4, closure="MOST", mol=mol
+        )
+        axs[0].plot(u, z)
+        axs[0].set_xlabel("u [m/s]")
+        axs[0].set_ylabel("z [m]")
+        axs[0].set_title("Profiles for MOST")
+        axs[1].plot(K, z)
+        axs[1].set_xlabel("K [m2 s-1]")
+        axs[1].set_ylabel("z [m]")
     plt.show()
 
     fig, axs = plt.subplots(2)
-    for tke in [0.2,0.5,1.0,1.5,2.0,2.5]:
+    for tke in [0.2, 0.5, 1.0, 1.5, 2.0, 2.5]:
         z, (u, v, K) = vertical_profiles(
-                n=100,
-                meas_height=5.0,
-                wind=(3.0, 1.0),
-                ustar=0.4,
-                closure = "OAAHOC",
-                tke = tke)
-        axs[0].plot(u,z)
-        axs[0].set_xlabel('u [m/s]')
-        axs[0].set_ylabel('z [m]')
-        axs[0].set_title('Profiles for One-and-a-half-order closure')
-        axs[1].plot(K,z)
-        axs[1].set_xlabel('K [m2 s-1]')
-        axs[1].set_ylabel('z [m]')
+            n=100,
+            meas_height=5.0,
+            wind=(3.0, 1.0),
+            ustar=0.4,
+            closure="OAAHOC",
+            tke=tke,
+        )
+        axs[0].plot(u, z)
+        axs[0].set_xlabel("u [m/s]")
+        axs[0].set_ylabel("z [m]")
+        axs[0].set_title("Profiles for One-and-a-half-order closure")
+        axs[1].plot(K, z)
+        axs[1].set_xlabel("K [m2 s-1]")
+        axs[1].set_ylabel("z [m]")
     plt.show()
-
