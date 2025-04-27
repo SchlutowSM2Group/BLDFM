0f8b4a8b4953179614f56ba6c36c0e263b63c832
ray-chew <chew@caltech.edu>
2025-04-27 11:19:47 -0700

    first completed documentation (#5)

--- runs/comparison_analytic.py
+++ runs/comparison_analytic.py
@@ -1,3 +1,7 @@
+"""
+Run script for comparing numerical and analytic solutions of concentration and flux.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -36,66 +40,67 @@ srf_conc, bg_conc, conc, flx = steady_state_transport_solver(
 diff_conc = (conc - conc_ana) / np.max(conc_ana)
 diff_flx = (flx - flx_ana) / np.max(flx_ana)
 
-shrink = 0.7
-cmap = "turbo"
+if '__name__' == "__main__":
+    shrink = 0.7
+    cmap = "turbo"
 
-fig, axs = plt.subplots(
-    2, 2, figsize=[10, 6], sharex=True, sharey=True, layout="constrained"
-)
+    fig, axs = plt.subplots(
+        2, 2, figsize=[10, 6], sharex=True, sharey=True, layout="constrained"
+    )
 
-plot = axs[0, 0].imshow(
-    conc, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
-)
+    plot = axs[0, 0].imshow(
+        conc, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+    )
 
-axs[0, 0].plot(src_pt[0], src_pt[1], "ro")
-axs[0, 0].set_title("Numerical concentration")
-axs[0, 0].set_ylabel("y [m]")
-axs[0, 0].xaxis.set_tick_params(labelbottom=False)
-cbar = fig.colorbar(plot, ax=axs[0, 0], shrink=shrink, location="bottom")
-cbar.formatter.set_powerlimits((0, 0))
-cbar.formatter.set_useMathText(True)
-cbar.set_label("a.u.")
+    axs[0, 0].plot(src_pt[0], src_pt[1], "ro")
+    axs[0, 0].set_title("Numerical concentration")
+    axs[0, 0].set_ylabel("y [m]")
+    axs[0, 0].xaxis.set_tick_params(labelbottom=False)
+    cbar = fig.colorbar(plot, ax=axs[0, 0], shrink=shrink, location="bottom")
+    cbar.formatter.set_powerlimits((0, 0))
+    cbar.formatter.set_useMathText(True)
+    cbar.set_label("a.u.")
 
-plot = axs[0, 1].imshow(
-    flx, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
-)
+    plot = axs[0, 1].imshow(
+        flx, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+    )
 
-axs[0, 1].plot(src_pt[0], src_pt[1], "ro")
-axs[0, 1].set_title("Numerical flux")
-axs[0, 1].xaxis.set_tick_params(labelbottom=False)
-axs[0, 1].yaxis.set_tick_params(labelleft=False)
-cbar = fig.colorbar(plot, ax=axs[0, 1], shrink=shrink, location="bottom")
-cbar.formatter.set_powerlimits((0, 0))
-cbar.formatter.set_useMathText(True)
-cbar.set_label("a.u. m/s")
+    axs[0, 1].plot(src_pt[0], src_pt[1], "ro")
+    axs[0, 1].set_title("Numerical flux")
+    axs[0, 1].xaxis.set_tick_params(labelbottom=False)
+    axs[0, 1].yaxis.set_tick_params(labelleft=False)
+    cbar = fig.colorbar(plot, ax=axs[0, 1], shrink=shrink, location="bottom")
+    cbar.formatter.set_powerlimits((0, 0))
+    cbar.formatter.set_useMathText(True)
+    cbar.set_label("a.u. m/s")
 
-cmap = "turbo"
+    cmap = "turbo"
 
-plot = axs[1, 0].imshow(
-    diff_conc, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
-)
+    plot = axs[1, 0].imshow(
+        diff_conc, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+    )
 
-# axs[1,0].plot(src_pt[0],src_pt[1],'ro')
-axs[1, 0].set_title("Relative difference to analytic concentration")
-axs[1, 0].set_xlabel("x [m]")
-axs[1, 0].set_ylabel("y [m]")
-cbar = fig.colorbar(plot, ax=axs[1, 0], shrink=shrink, location="bottom")
-# cbar.set_label('‰')
-cbar.formatter.set_powerlimits((0, 0))
-cbar.formatter.set_useMathText(True)
+    # axs[1,0].plot(src_pt[0],src_pt[1],'ro')
+    axs[1, 0].set_title("Relative difference to analytic concentration")
+    axs[1, 0].set_xlabel("x [m]")
+    axs[1, 0].set_ylabel("y [m]")
+    cbar = fig.colorbar(plot, ax=axs[1, 0], shrink=shrink, location="bottom")
+    # cbar.set_label('‰')
+    cbar.formatter.set_powerlimits((0, 0))
+    cbar.formatter.set_useMathText(True)
 
-plot = axs[1, 1].imshow(
-    diff_flx, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
-)
+    plot = axs[1, 1].imshow(
+        diff_flx, origin="lower", cmap=cmap, extent=[0, domain[0], 0, domain[1]]
+    )
 
-# axs[1,1].plot(src_pt[0],src_pt[1],'ro')
-axs[1, 1].set_title("Relative difference to analytic flux")
-axs[1, 1].set_xlabel("x [m]")
-axs[1, 1].yaxis.set_tick_params(labelleft=False)
-cbar = fig.colorbar(plot, ax=axs[1, 1], shrink=shrink, location="bottom")
-# cbar.set_label('‰')
-cbar.formatter.set_powerlimits((0, 0))
-cbar.formatter.set_useMathText(True)
+    # axs[1,1].plot(src_pt[0],src_pt[1],'ro')
+    axs[1, 1].set_title("Relative difference to analytic flux")
+    axs[1, 1].set_xlabel("x [m]")
+    axs[1, 1].yaxis.set_tick_params(labelleft=False)
+    cbar = fig.colorbar(plot, ax=axs[1, 1], shrink=shrink, location="bottom")
+    # cbar.set_label('‰')
+    cbar.formatter.set_powerlimits((0, 0))
+    cbar.formatter.set_useMathText(True)
 
-# plt.show()
-plt.savefig("plots/comparison_analytic.png", dpi=300)
+    # plt.show()
+    plt.savefig("plots/comparison_analytic.png", dpi=300)
