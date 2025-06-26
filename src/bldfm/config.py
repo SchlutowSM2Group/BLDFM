ac0fb09c3b753d1b995a5acce3f168bb3df8a4f4
Mark Schlutow <mschlutow@bgc-jena.mpg.de>
2025-06-26 14:30:18 +0200

    blacked

--- src/bldfm/config.py
+++ src/bldfm/config.py
@@ -2,7 +2,6 @@
 Provides global variables for BLDFM.
 """
 
-# Number of threads for parallel computation. 
-# NUM_THREADS=1 equivalent to serial 
+# Number of threads for parallel computation.
+# NUM_THREADS=1 equivalent to serial
 NUM_THREADS = 1
-
